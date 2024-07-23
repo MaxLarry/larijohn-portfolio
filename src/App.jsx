46142c8f96494c17dade56086d7cd480e69c6606
YlarHeartbb <202080010@psu.palawan.ph>
2024-07-23 23:31:06 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+ //alaws pa laman kasi nasa kabila lahat dahil tinatamad ako mag git.
+ // lipat ko lang dito mga natapos ko sa kabilang file project.
+ // pang github lang ito at sa kabila ako mag dedevelop para malinis, para may ma push din ako hehe.
+}
+
+export default App
